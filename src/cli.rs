c54fefd44b67de5b6201468074731d867a887db7
Bryant Biggs <bryantbiggs@gmail.com>
2024-03-21 17:53:35 -0400

    chore: Move cli bits to `cli.rs`

---
+++ src/cli.rs
@@ -0,0 +1,83 @@
+use std::path::PathBuf;
+
+use anstyle::{AnsiColor, Color, Style};
+use anyhow::Result;
+use clap::{builder::Styles, Args, Parser, Subcommand};
+use clap_verbosity_flag::Verbosity;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+/// Styles for CLI
+fn get_styles() -> Styles {
+  Styles::styled()
+    .header(
+      Style::new()
+        .bold()
+        .underline()
+        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
+    )
+    .literal(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+    .usage(
+      Style::new()
+        .bold()
+        .underline()
+        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
+    )
+    .placeholder(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Magenta))))
+}
+
+#[derive(Debug, Parser)]
+#[command(author, about, version)]
+#[command(author, version, about)]
+#[command(propagate_version = true)]
+#[command(styles=get_styles())]
+pub struct Cli {
+  #[command(subcommand)]
+  pub command: Commands,
+
+  #[clap(flatten)]
+  pub verbose: Verbosity,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+  /// Get module downloads from Terraform registry
+  GetDownloads(Download),
+}
+
+#[derive(Args, Debug, Deserialize, Serialize)]
+pub struct Download {
+  /// The module to collect download data for
+  #[clap(short, long)]
+  module: String,
+
+  /// Path where the collected data outputs will be written to
+  #[clap(short, long)]
+  path: Option<PathBuf>,
+}
+
+impl Download {
+  pub async fn get(&self) -> Result<()> {
+    let url = Url::parse_with_params(
+      format!(
+        "https://registry.terraform.io/v2/modules/terraform-aws-modules/{}/aws",
+        self.module
+      )
+      .as_str(),
+      &[("include", "module-versions")],
+    )?;
+
+    let resp = Client::builder()
+      .user_agent("Module Download Data")
+      .build()?
+      .get(url)
+      .send()
+      .await?;
+
+    let response: crate::data::Response = resp.json().await?;
+    println!("{:#?}", response);
+
+    Ok(())
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,15 @@
 use std::env;
 
-use anstyle::{AnsiColor, Color, Style};
 use anyhow::Result;
-use clap::{builder::Styles, Parser, Subcommand};
-use clap_verbosity_flag::Verbosity;
+use clap::Parser;
+use module_download_data::cli as mdd;
 use tracing_log::AsTrace;
 use tracing_subscriber::FmtSubscriber;
-
-/// Styles for CLI
-fn get_styles() -> Styles {
-  Styles::styled()
-    .header(
-      Style::new()
-        .bold()
-        .underline()
-        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
-    )
-    .literal(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
-    .usage(
-      Style::new()
-        .bold()
-        .underline()
-        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
-    )
-    .placeholder(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Magenta))))
-}
-
-#[derive(Debug, Parser)]
-#[command(author, about, version)]
-#[command(author, version, about)]
-#[command(propagate_version = true)]
-#[command(styles=get_styles())]
-pub struct Cli {
-  #[command(subcommand)]
-  pub command: Commands,
-
-  #[clap(flatten)]
-  pub verbose: Verbosity,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum Commands {
-  /// Get module downloads from Terraform registry
-  GetDownloads(module_download_data::Download),
-}
 
 #[cfg(not(tarpaulin_include))]
 #[tokio::main]
 async fn main() -> Result<()> {
-  let cli = Cli::parse();
+  let cli = mdd::Cli::parse();
   let subscriber = FmtSubscriber::builder()
     .with_max_level(cli.verbose.log_level_filter().as_trace())
     .without_time()
@@ -59,6 +20,6 @@ async fn main() -> Result<()> {
   let _cur_dir = cur_exe.parent().unwrap().parent().unwrap().parent().unwrap();
 
   match &cli.command {
-    Commands::GetDownloads(download) => download.get().await,
+    mdd::Commands::GetDownloads(download) => download.get().await,
   }
 }
