4f14a100370cec6d97876b42043f72fbe9cf19cd
Bryant Biggs <bryantbiggs@gmail.com>
2024-03-23 13:47:00 -0400

    chore: Re-update charts and ensure charts are updated after collecting data going forward

--- collect.sh
+++ collect.sh
@@ -2,8 +2,13 @@
 
 cargo build --release
 
+# Collect data for all modules
 gh repo list terraform-aws-modules --source --limit 100 --json name --jq '.[].name' | \
   grep terraform-aws- | \
   grep -v s3-object | \
   sed 's/terraform-aws-//g' | \
   xargs -I {} target/release/tmd collect-data --module {}
+
+# Update graphs
+
+target/release/tmd graph
